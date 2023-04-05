984755ef101c30e2a7ef3b769518c638daa44100
m-4morim <m.4morim@outlook.com>
2023-04-04 21:08:38 -0300

    :sparkles: feat: Criando função aoDeletar

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -3,14 +3,15 @@ import './time.css'
 
 const Time = ({ time, colaboradores }) => {
     return (
-
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
+                {colaboradores.map((colaborador, indice) => {
+                    console.log('renderizando colaborador');
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />;
+                })}
             </div>
         </section>
-
     )
 }
 
